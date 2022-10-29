29582bf210d49e06376588b7e4218c824e7f56e4
HongboooLiu <98497410+HongboooLiu@users.noreply.github.com>
2022-10-29 01:37:06 -0400

    Update cipher_hl3522.py

--- cipher_hl3522/src/cipher_hl3522/cipher_hl3522.py
+++ cipher_hl3522/src/cipher_hl3522/cipher_hl3522.py
@@ -1,4 +1,16 @@
 def cipher(text, shift, encrypt=True):
+    '''
+    The Caesar cipher is one of the simplest and most widely known encryption techniques. In short, each letter is           replaced by a letter some fixed number of positions down the alphabet. Apparently, Julius Caesar used it in his         private correspondence.
+    ---------
+    Input:
+    text: string letter text
+    shift: The number of position each letter shift down the alphabet
+    encrypt: True; The cipher function shift the letters down the alphabet
+          False; The cipher function shift the letters up the alphabet
+    ---------
+    Output: 
+    New text after the cipher function
+    '''
     alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     new_text = ''
     for c in text:
